e49de3a09bf5976d16997a13a182ca062eecea61
JhorsPineda <jhorspineda30@gmail.com>
2024-03-14 20:56:23 -0500

    Cambie la conexion de la base datos local a un servidor RailWay

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php
 
-    $host = "localhost";
+    $host = "viaduct.proxy.rlwy.net";
     $user = "root";
-    $clave = "";
-    $bd = "gymtonic";
+    $clave = "PIfwpDKQkdHSYRIzaoZesjAeuQgXXOmW";
+    $bd = "railway";
+    $port = "34769";
 
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
+    $conexion = mysqli_connect($host,$user,$clave,$bd,$port);
     if (mysqli_connect_errno()){
         echo "No se pudo conectar a la base de datos";
         exit();
